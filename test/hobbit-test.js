7988aadc9a1cd741db2204e46988e2eb28920260
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-22 09:10:39 -0700

    Pass third hobbit test

--- test/hobbit-test.js
+++ test/hobbit-test.js
@@ -17,7 +17,7 @@ describe('Hobbit', function() {
     assert.equal(hobbit.name, 'Bilbo');
   });
 
-  it.skip('should have an unadventurous disposition', function() {
+  it('should have an unadventurous disposition', function() {
     var hobbit = new Hobbit('Samwise');
     assert.equal(hobbit.disposition, 'homebody');
   });
