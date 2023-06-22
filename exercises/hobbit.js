82270e1ba027af394451b0f6b5b906602a5f344a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-22 09:30:42 -0700

    Pass ninth hobbit test

--- exercises/hobbit.js
+++ exercises/hobbit.js
@@ -4,12 +4,17 @@ class Hobbit {
     this.disposition = 'homebody';
     this.age = 0;
     this.adult = false;
+    this.isShort = true;
+    this.old = false;
   }
 
   celebrateBirthday() {
     this.age++
     if (this.age > 32) {
       this.adult = true;
+    }
+    if (this.age > 100) {
+      this.old = true;
     }
   }
 }
